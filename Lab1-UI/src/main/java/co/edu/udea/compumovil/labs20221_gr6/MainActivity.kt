1ca4cf11b63d63b12e4ba3c372b583e5c734f21f
royk <roy.maestre@udea.edu.co>
2022-06-29 23:21:09 -0500

    First try with contactActivity

--- Lab1-UI/src/main/java/co/edu/udea/compumovil/labs20221_gr6/MainActivity.kt
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/labs20221_gr6/MainActivity.kt
@@ -1,11 +1,21 @@
 package co.edu.udea.compumovil.labs20221_gr6
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    fun nextButton(view: View){
+        val s = "getString(R.string.next)";
+        val intent = Intent(this, ContactDataActivity::class.java);
+        startActivity(intent);
     }
 }
